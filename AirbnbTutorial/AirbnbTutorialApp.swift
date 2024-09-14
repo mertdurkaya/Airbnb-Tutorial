d0fef3de007656772f3a3e4a63bb3fae603c3562
Mert Durkaya <mertdurkaya@gmail.com>
2024-09-14 16:12:55 +0300

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Mert Durkaya on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
